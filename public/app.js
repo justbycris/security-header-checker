bfd6f4829e72b4b1d16c4f097cda3fc896f79e08
Cristi Gutierrez Name <65434648+justbycris@users.noreply.github.com>
2026-02-03 20:17:43 -0800

    updates

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+const log = console.log; 
 let userURL = document.getElementById('urlInput'); 
 let checkBTN = document.getElementById('checkBtn'); 
 let errorMsg = document.getElementById('error-msg'); 
@@ -6,27 +7,62 @@ let results = document.getElementById('results');
 let resultsURL = document.getElementById('summary-url');
 let resultsIP = document.getElementById('results-ip');
 let score = document.getElementById('results-score'); 
-const log = console.log; 
 
-checkBTN.addEventListener('click', async () => {
-   
-    userURL.addEventListener('input', () => {
+
+
+//Validation - if input value is empty throw an error 
+function validateUserInput(userValue){
+    validInput = false;
+    if(!userValue){
+        userURL.style.border = '1px solid red';
+        errorMsg.style.display = 'block';
+        errorMsg.innerText = 'Please enter a URL...'
+        results.style.display = 'none';
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// UI - check if user is typing and clean css styles
+function userIsTyping(input){
+    input.addEventListener('input', () => {
         if(userURL.value != ''){
         userURL.style.border = 'none';
         errorMsg.style.display = 'none';
         errorMsg.innerText = ''
         }
     })
+}
 
-    //If input value is empty throw an error 
-    if(!userURL.value){
-        userURL.style.border = '1px solid red';
-        errorMsg.style.display = 'block';
-        errorMsg.innerText = 'Please enter a URL...'
-        results.style.display = 'none';
+// UI - display results 
+function displayResultsUI(response){
+    //Review
+    userURL.style.border = '4px solid green'
+    results.style.visibility = 'visible';
+    resultsIP.innerText = response.ip;
+    resultsURL.innerText = `URL: ${userURL.value}`;
+    score.innerText = response.analysis.score;
+    //Results details
+ 
+}
+
+//UI - Show error if needed
+function logError(code, error){
+    serverErr.innerText = `${code}: ${error}`;
+    serverErr.style.display = 'block'
+    userURL.style.border = '1px solid red';
+}
+
+
+//SCAN URL button event listener
+checkBTN.addEventListener('click', async () => {
+    validInput = false;
+    userIsTyping(userURL)
+    const isValid = validateUserInput(userURL.value)
+    if(!isValid){
         return;
-    } 
-  
+    }
 
     //Fetch server response and show in the frontend 
     try {
@@ -34,19 +70,14 @@ checkBTN.addEventListener('click', async () => {
             url: userURL.value
         });
 
-            log(response.data)
-            userURL.style.border = '2px solid green'
-            results.style.visibility = 'visible';
-            resultsIP.innerText = response.data.ip;
-            resultsURL.innerText = userURL.value;
-            score.innerText = response.data.analysis.score;
-        
+        log(`Response: ${response.data}`)
+        displayResultsUI(response.data)
+
         console.log(response.data);
         return response;
     } catch (error) {
-        serverErr.innerText = `${error.code}: ${error.message}`;
-        serverErr.style.display = 'block'
-        userURL.style.border = '1px solid red';
+        //Server ERROR and UI error
+        logError(error.code, error.message)
         console.error(error);
     }
 
